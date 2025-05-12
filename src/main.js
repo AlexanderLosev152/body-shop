47ef1cf342113942a22d32b5e23c213840c2a8a7
Alexander <93211887+AlexanderLosev152@users.noreply.github.com>
2025-05-12 06:53:31 +0300

    add swiper

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
 import './style.scss';
-console.log('hello vite');
+// import '../node_modules/swiper/swiper-bundle';
+// console.log('hello vite');
 
-const btn = document.querySelector('.burger');
-const nav = document.querySelector('.nav');
-const links = document.querySelectorAll('.nav ul li a');
+// const btn = document.querySelector('.burger');
+// const nav = document.querySelector('.nav');
+// const links = document.querySelectorAll('.nav ul li a');
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open');
-  nav.classList.toggle('open');
+// btn.addEventListener('click', () => {
+//   btn.classList.toggle('open');
+//   nav.classList.toggle('open');
 
-  if (btn.classList.contains('open')) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
-  }
-});
+//   if (btn.classList.contains('open')) {
+//     document.body.style.overflow = 'hidden';
+//   } else {
+//     document.body.style.overflow = '';
+//   }
+// });
 
-links.forEach((el) => {
-  el.addEventListener('click', () => {
-    btn.classList.remove('open');
-    nav.classList.remove('open');
-    document.body.style.overflow = '';
-  });
+// links.forEach((el) => {
+//   el.addEventListener('click', () => {
+//     btn.classList.remove('open');
+//     nav.classList.remove('open');
+//     document.body.style.overflow = '';
+//   });
+// });
+
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
 });
